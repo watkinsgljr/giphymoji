7b106b340f3a662e47216794a11c84fba22eb5dc
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-03-28 18:48:56 -0500

    api LINK AND BUTTON CLICK EVENTS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,11 @@
 
 
-let queries = ['happy', 'excited', 'angry', 'suspense', 'sad', 'laughing', 'scared', 'alarmed', 'suprised', 'depressed', 'elated', 'confused', 'frustrated', 'annoyed', 'flustered',]
+//-----------------------GLOBAL VARIABLES-------------------------------------------------------------------------
 
+
+let queries = ['happy', 'excited', 'angry', 'suspense', 'sad', 'laughing', 'scared', 'alarmed', 'suprised', 'depressed', 'elated', 'confused', 'frustrated', 'annoyed', 'flustered',]
+let clickValue;
+//-----------------------BUTTON GENERATION---------------------------------------------------------------------
 
 for (i = 0; i < queries.length; i++) {
     queryButton = $("<button>");
@@ -10,6 +14,31 @@ for (i = 0; i < queries.length; i++) {
     queryButton.text(queries[i]);
     $("#button-container").append(queryButton);
 
-}
 
-// API KEY TzGZCgR5pjsTx9K7Re8DiK3EInjsmd9d
+}
+//------------------------BUTTON CLICK EVENTS-------------------------------------------------------------------
+
+$('.query-button').click(function() {
+    clickValue = $(this).text();
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + clickValue + "&api_key=TzGZCgR5pjsTx9K7Re8DiK3EInjsmd9d&limit=8");
+    xhr.done(function(data) { console.log("success got data", data); });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------API KEY----------------------------------------------------------
+// TzGZCgR5pjsTx9K7Re8DiK3EInjsmd9d
